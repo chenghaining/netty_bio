6d961548d1305a2e0d089a448127c8496b41a483
chenghn <chenghn@email.finedo.cn>
2022-12-29 10:29:21 +0800

    hotfix commit2

--- src/main/java/com/chn/netty/git/Gittest.java
+++ src/main/java/com/chn/netty/git/Gittest.java
@@ -6,5 +6,6 @@ public class Gittest {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello hotfix");
     }
 }
